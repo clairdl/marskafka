796a3b403cab8ffbe9d54d24ab89c938dd18e0f3
clairdl <52753311+clairdl@users.noreply.github.com>
2021-09-29 12:00:51 -0700

    nasa api request works

--- pushkin/producer.py
+++ pushkin/producer.py
@@ -1,27 +1,53 @@
 import os
 import sched, time
 
-from kafka import KafkaProducer
 import requests
+from dotenv import load_dotenv
+from kafka import KafkaProducer
+import json
+
+load_dotenv()
 
 KAFKA_BROKER_URL = os.environ.get("KAFKA_BROKER_URL")
 producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER_URL)
 
+# format url
+def fReqUrl(rover, sol):
+    return f"https://api.nasa.gov/mars-photos/api/v1/rovers/{rover}/photos?sol={sol}&api_key={os.getenv('NASA_API_KEY')}"
+
+
 # get images from nasa api and clean the response
 def getImages():
-  pass
+    r = requests.get(fReqUrl("curiosity", 2000))
+    r = r.json()
+
+    res = [
+        {"id": i["id"], "camera": i["camera"]["name"], "img_src": i["img_src"]}
+        for i in r["photos"]
+    ]
+    # res = [{
+    #   "id": int
+    #   "camera": string
+    #   "img_src": string
+    # }...]
+
+    return res
+
 
 # SEND IT
 def postToKafkaTopic():
-  pass
+    pass
+
 
 # schedule another mainLoop tmr
 def addCronjob():
-  pass
+    pass
 
-# pipe get images to kafka, handle errors, etc
+
+# pipe images to kafka, handle errors, etc
 def main():
-  pass
+    pass
+
 
 if __name__ == "__main__":
-  main()
+    print(getImages())
