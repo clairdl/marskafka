ef009571fa8c236b2bf6d07933a8c233a3968dd4
clairdl <52753311+clairdl@users.noreply.github.com>
2021-09-29 12:50:42 -0700

    move main procedure + whilelist 'good' cameras

--- pushkin/producer.py
+++ pushkin/producer.py
@@ -6,11 +6,6 @@ from dotenv import load_dotenv
 from kafka import KafkaProducer
 import json
 
-load_dotenv()
-
-KAFKA_BROKER_URL = os.environ.get("KAFKA_BROKER_URL")
-producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER_URL)
-
 # format url
 def fReqUrl(rover, sol):
     return f"https://api.nasa.gov/mars-photos/api/v1/rovers/{rover}/photos?sol={sol}&api_key={os.getenv('NASA_API_KEY')}"
@@ -18,25 +13,22 @@ def fReqUrl(rover, sol):
 
 # get images from nasa api and clean the response
 def getImages():
+    # todo: better sampling from the api, support more cams and rovers
+    whitelist = {'FHAZ', 'RHAZ', 'NAVCAM', 'PANCAM' 'FRONT_HAZCAM_LEFT_A', 'FRONT_HAZCAM_RIGHT_A', 'REAR_HAZCAM_LEFT', 'REAR_HAZCAM_RIGHT' }
     r = requests.get(fReqUrl("curiosity", 2000))
     r = r.json()
 
     res = [
-        {"id": i["id"], "camera": i["camera"]["name"], "img_src": i["img_src"]}
-        for i in r["photos"]
+        {"sol": i["sol"], "camera": i["camera"]["name"], "img_src": i["img_src"]}
+        for i in r["photos"] if i["camera"]["name"] in whitelist
     ]
     # res = [{
-    #   "id": int
-    #   "camera": string
-    #   "img_src": string
+    #   "sol": int
+    #   "camera": str
+    #   "img_src": str
     # }...]
-
+    print(res)
     return res
-
-
-# SEND IT
-def postToKafkaTopic():
-    pass
 
 
 # schedule another mainLoop tmr
@@ -44,10 +36,15 @@ def addCronjob():
     pass
 
 
-# pipe images to kafka, handle errors, etc
-def main():
-    pass
-
-
 if __name__ == "__main__":
-    print(getImages())
+    load_dotenv()
+    # had to hardcode the server address frmo docker config
+    # because KAFKA_BROKER_URL is not in my env for some reason
+    producer = KafkaProducer(
+        bootstrap_servers="0.0.0.0:9092",
+        value_serializer=lambda v: json.dumps(v).encode(),
+    )
+
+    for i in getImages():
+        postToKafkaTopic(getImages())
+        producer.send("queue.bwimgs", value=message.encode())
